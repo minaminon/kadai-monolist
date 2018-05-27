c7207a3e672c9bfc002513acc7a466e72910fb09
naoto.mnmn <houganhalf@yahoo.co.jp>
2018-05-27 14:28:27 +0000

    want

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,27 +9,15 @@ class ItemsController < ApplicationController
         hits: 20,
       })
       results.each do|result|
-        item=Item.new(read(result))
+        item=Item.find_or_initialize_by(read(result))
         @items << item
       end
     end
   end
   
-private
-  
-  def read(result)
-    code=result['itemCode']
-    name=result['itemName']
-    url=result['itemUrl']
-    image_url=result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128×128','')
-    
-    return{
-      code: code,
-      name: name,
-      url: url,
-      image_url: image_url,
-    }
-    
+  def show
+    @item=Item.find(params[:id])
+    @want_users=@item.want_users
   end
   
 end
